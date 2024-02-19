0489d4abc11b443e98348f3aec94fc27b278e9dd
Zach Wong <zach.wong@uniswap.org>
2024-02-19 11:17:53 -0500

    redo SQL pull, establish full PENDLE-ETH nance price series

--- CEX_data_pull.sql
+++ CEX_data_pull.sql
@@ -3,7 +3,8 @@
 SELECT
   timestamp,
   symbol,
-  price
+  avg(price) as price -- averaging once per timestamp (which is hella small anyways) to cut down on the # rows.
 FROM `cex.binance_trades_SPOT`
-where symbol in ('PENDLEUSDT', 'USDCUSDT')
+where symbol in ('PENDLEUSDT', 'ETHUSDT')
+group by 1, 2
 order by 1 desc
